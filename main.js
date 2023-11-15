6b19e546f72bef98198411739fd5ba4856345de6
jonathan <jhontombe33@gmail.com>
2023-11-15 15:09:58 -0500

    Creación de objeto de películas J.T

---
+++ main.js
@@ -0,0 +1,137 @@
+const peliculas = [
+    {
+        titulo: 'Star Wars el ascenso de skywalkers',
+        fecha: '2019',
+        categoria: 'Ciencia Ficcion',
+        sipnosis:
+            'Finn y Poe guían a la Resistencia para detener los planes de la Primera Orden para formar un nuevo imperio, mientras Rey anticipa un enfrentamiento inevitable con Kylo Ren.',
+        trailer: 'https://youtu.be/Izme__ZsURY',
+        imagen: 'assets/Images/Movies/1.jpg'
+    },
+    {
+        titulo: 'Storm',
+        fecha: '2022',
+        categoria: 'Ciencia Ficcion',
+        sipnosis:
+            'El hijo de Elin muere en un accidente por ahogamiento, pero junto con su hija Storm (10) intenta reiniciar, pero cuando comienzan los rumores de que Storm empujó a su hermano al agua tiene que proteger a su hija y aun así encontrar la verdad.',
+        trailer: 'https://youtu.be/xBH25XxM-7g',
+        imagen: 'assets/Images/Movies/2.jpg'
+    },
+    {
+        titulo: '1917',
+        fecha: '2020',
+        categoria: 'Accion',
+        sipnosis:
+            'Durante la Primera Guerra Mundial, dos jóvenes soldados británicos reciben una orden, aparentemente, imposible de ejecutar: en una carrera contra el reloj, deben infiltrarse en territorio enemigo para entregar un mensaje que podría salvar a miles de compañeros',
+        trailer: 'https://youtu.be/YrbdN5zaouU',
+        imagen: 'assets/Images/Movies/3.jpg'
+    },
+    {
+        titulo: 'Los vengadores',
+        fecha: '2012',
+        categoria: 'Accion',
+        sipnosis:
+            'Cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial, Nick Fury, director de la Agencia SHIELD, decide reclutar a un equipo para salvar al mundo de un desastre casi seguro. Adaptación del cómic de Marvel Los Vengadores, el legendario grupo de superhéroes formado por Ironman, Hulk, Thor y el Capitán América entre otros.',
+        trailer: 'https://youtu.be/HQIiYqOVTWo',
+        imagen: 'assets/Images/Movies/4.jpg'
+    },
+    {
+        titulo: 'Rampage:davastacion',
+        fecha: '2018',
+        categoria: 'Accion',
+        sipnosis:
+            'Un gorila, un lobo y una serpiente sufren una mutación genética y se convierten en seres gigantescos y violentos. El primatólogo que cuidaba del gorila y una experta en genética tratan de detenerlos y, al mismo tiempo, evitar que sufran daños.',
+        trailer: 'https://youtu.be/4NgyVuh3Tcw',
+        imagen: 'assets/Images/Movies/5.jpg'
+    },
+    {
+        titulo: 'Oblivion:el tiempo del olvido',
+        fecha: '2013',
+        categoria: 'Ciencia Ficcion',
+        sipnosis:
+            'Jack Harper y Victoria Olsen son los únicos habitantes de la Tierra, destruida hace sesenta años. Harper, un mecánico de aviación, rescata a una mujer antes de abandonar la Tierra para siempre e inicia una batalla para salvar a la humanidad.',
+        trailer: 'https://youtu.be/Pt7TGR8wwdw',
+        imagen: 'assets/Images/Movies/6.jpg'
+    },
+    {
+        titulo: 'Hobbit',
+        fecha: '2012',
+        categoria: 'Ciencia Ficcion',
+        sipnosis:
+            'La trilogía de El hobbit, adaptación cinematográfica basada en la novela homónima, comprende tres películas épicas de fantasía, acción y aventuras: El hobbit: un viaje inesperado, El hobbit: la desolación de Smaug y El hobbit: la batalla de los Cinco Ejércitos. ',
+        trailer: 'https://youtu.be/sI8OQRXhcZE',
+        imagen: 'assets/Images/Movies/7.jpg'
+    },
+    {
+        titulo: 'Due date',
+        fecha: '2010',
+        categoria: 'Comedia',
+        sipnosis:
+            'Desesperado por llegar a casa para acompañar a su esposa embarazada, un arquitecto viaja desde Atlanta hasta Los Ángeles junto a un excéntrico e irritante desconocido. ',
+        trailer: 'https://youtu.be/1p3NnJ_oiE0',
+        imagen: 'assets/Images/Movies/8.jpg'
+    },
+    {
+        titulo: 'Kickc Ass 2',
+        fecha: '2010',
+        categoria: 'Comedia',
+        sipnosis:
+            'Dave un estudiante de instituto aficionado a los cómics y que siempre pasa desapercibido, decide un día convertirse en superhéroe aunque no tenga superpoderes, no haya seguido un duro entrenamiento y ni siquiera tenga una significativa razón para ello.',
+        trailer: 'https://youtu.be/Kt39nS-kJ9U',
+        imagen: 'assets/Images/Movies/9.jpg'
+    },
+    {
+        titulo: 'Emoji Movie',
+        fecha: '2017',
+        categoria: 'Comedia',
+        sipnosis:
+            'Gene, un emoji con varias expresiones, pide a su amigo Hi-5 y al desencriptador Jailbreak que le ayuden a convertirse en un emoji de una cara, como todos sus amigos. Durante su aventura recorren varias aplicaciones y descubren que el teléfono en el que viven está en peligro.',
+        trailer: 'https://www.youtube.com/watch?v=7hKwPHRLz58',
+        imagen: 'assets/Images/Movies/10.jpg'
+    },
+    {
+        titulo: '¿Qué pasó ayer?',
+        fecha: '2009',
+        categoria: 'Comedia',
+        sipnosis:
+            'Una pareja se muda a una mansión para convertirla en un hotel, pero no son conscientes del peligro que les espera. Sin embargo, cuando ella es poseída por un espíritu demoníaco, su compañero decide salvarla.',
+        trailer: 'https://www.youtube.com/watch?v=wnNgGp1KVWQ',
+        imagen: 'assets/Images/Movies/11.jpg'
+    },
+    {
+        titulo: 'Paths of glory',
+        fecha: '1957',
+        categoria: 'Accion',
+        sipnosis:
+            'En la Francia de 1916, el general Boulard ordena la conquista alemana y encarga esa misión al general Mireau. Sin embargo, el ataque resulta un fracaso y el alto mando militar decide imponer al regimiento un terrible castigo.',
+        trailer: 'https://www.youtube.com/watch?v=nmDA60X-f_A',
+        imagen: 'assets/Images/Movies/12.jpg'
+    },
+    {
+        titulo: 'LEGO Batman: la película',
+        fecha: '2017',
+        categoria: 'Ciencia Ficcion',
+        sipnosis:
+            'El hijo de Elin muere en un accidente por ahogamiento, pero junto con su hija Storm (10) intenta reiniciar, pero cuando comienzan los rumores de que Storm empujó a su hermano al agua tiene que proteger a su hija y aun así encontrar la verdad.',
+        trailer: 'https://www.youtube.com/watch?v=JHxhTOxX_bs',
+        imagen: 'assets/Images/Movies/13.jpg'
+    },
+    {
+        titulo: 'Aquaman',
+        fecha: '2022',
+        categoria: 'Ciencia Ficcion',
+        sipnosis:
+            'Aquaman debe recuperar el legendario Tridente de Atlan para salvar a la ciudad subacuática de Atlantis, y al mundo de la superficie, de su hermano hambriento de poder.',
+        trailer: 'https://youtu.be/D3WCrSnCLEM',
+        imagen: 'assets/Images/Movies/14.jpg'
+    },
+    {
+        titulo: 'Sonic La Pelicula',
+        fecha: '2020',
+        categoria: 'Comedia',
+        sipnosis:
+            'Sonic intenta atravesar las complejidades de la vida en la Tierra con su nuevo mejor amigo, un humano llamado Tom Wachowski. Deberán unir sus fuerzas para evitar que el malvado Dr. Robotnik capture a Sonic y use sus poderes para dominar el mundo.',
+        trailer: 'https://www.youtube.com/watch?v=XPqAai3WBag',
+        imagen: 'assets/Images/Movies/15.jpg'
+    }
+]
