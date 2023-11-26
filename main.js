eca4fb796089290e72ccd3183965d58a89d0ff83
jonathan <jhontombe33@gmil.com>
2023-11-25 21:00:25 -0500

    agregando style a las peliculas y series

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const peliculas = [
   {
-    titulo: 'Star Wars el ascenso de skywalkers',
+    titulo: 'Star Wars ',
     fecha: '2019',
     categoria: 'Ciencia Ficcion',
     sipnosis:
-      'Finn y Poe guían a la Resistencia para detener los planes de la Primera Orden para formar un nuevo imperio, mientras Rey anticipa un enfrentamiento inevitable con Kylo Ren.',
+      'Finn y Poe guían a la Resistencia para detener los planes de la Primera Orden para formar un nuevo.',
     trailer: 'https://www.youtube.com/embed/mAGcR6ZqKlQ?si=0lCojQvEBx4PpQVB',
     imagen: 'assets/Images/Movies/1.jpg'
   },
@@ -13,7 +13,7 @@ const peliculas = [
     fecha: '2022',
     categoria: 'Ciencia Ficcion',
     sipnosis:
-      'El hijo de Elin muere en un accidente por ahogamiento, pero junto con su hija Storm (10) intenta reiniciar, pero cuando comienzan los rumores de que Storm empujó a su hermano al agua tiene que proteger a su hija y aun así encontrar la verdad.',
+      'El hijo de Elin muere en un accidente por ahogamiento, pero junto con su hija Storm (10) intenta reiniciar, pero.',
     trailer: 'https://www.youtube.com/embed/xBH25XxM-7g',
     imagen: 'assets/Images/Movies/2.jpg'
   },
@@ -22,7 +22,7 @@ const peliculas = [
     fecha: '2020',
     categoria: 'Accion',
     sipnosis:
-      'Durante la Primera Guerra Mundial, dos jóvenes soldados británicos reciben una orden, aparentemente, imposible de ejecutar: en una carrera contra el reloj, deben infiltrarse en territorio enemigo para entregar un mensaje que podría salvar a miles de compañeros',
+      'Durante la Primera Guerra Mundial, dos jóvenes soldados británicos reciben una orden, aparentemente, imposible de  ',
     trailer: 'https://www.youtube.com/embed/YrbdN5zaouU',
     imagen: 'assets/Images/Movies/3.jpg'
   },
@@ -31,25 +31,25 @@ const peliculas = [
     fecha: '2012',
     categoria: 'Accion',
     sipnosis:
-      'Cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial, Nick Fury, director de la Agencia SHIELD, decide reclutar a un equipo para salvar al mundo de un desastre casi seguro.',
+      'Cuando un enemigo inesperado surge como una gran amenaza para la seguridad mundial, Nick Fury, director de la.',
     trailer: 'https://www.youtube.com/embed/HQIiYqOVTWo',
     imagen: 'assets/Images/Movies/4.jpg'
   },
   {
-    titulo: 'Rampage:davastacion',
+    titulo: 'Rampage',
     fecha: '2018',
     categoria: 'Accion',
     sipnosis:
-      'Un gorila, un lobo y una serpiente sufren una mutación genética y se convierten en seres gigantescos y violentos. El primatólogo que cuidaba del gorila y una experta en genética tratan de detenerlos y, al mismo tiempo, evitar que sufran daños.',
+      'Un gorila, un lobo y una serpiente sufren una mutación genética y se convierten en seres gigantescos y.',
     trailer: 'https://www.youtube.com/embed/4NgyVuh3Tcw',
     imagen: 'assets/Images/Movies/5.jpg'
   },
   {
-    titulo: 'Oblivion:el tiempo del olvido',
+    titulo: 'Oblivion',
     fecha: '2013',
     categoria: 'Ciencia Ficcion',
     sipnosis:
-      'Jack Harper y Victoria Olsen son los únicos habitantes de la Tierra, destruida hace sesenta años. Harper, un mecánico de aviación, rescata a una mujer antes de abandonar la Tierra para siempre e inicia una batalla para salvar a la humanidad.',
+      'Jack Harper y Victoria Olsen son los únicos habitantes de la Tierra, destruida hace sesenta años. Harper, un. ',
     trailer: 'https://www.youtube.com/embed/Pt7TGR8wwdw',
     imagen: 'assets/Images/Movies/6.jpg'
   },
@@ -58,7 +58,7 @@ const peliculas = [
     fecha: '2012',
     categoria: 'Ciencia Ficcion',
     sipnosis:
-      'La trilogía de El hobbit, adaptación cinematográfica basada en la novela homónima, comprende tres películas épicas de fantasía, acción y aventuras: El hobbit: un viaje inesperado, El hobbit: la desolación de Smaug y El hobbit: la batalla de los Cinco Ejércitos. ',
+      'La trilogía de El hobbit, adaptación cinematográfica basada en la novela homónima, comprende tres películas.',
     trailer: 'https://www.youtube.com/embed/sI8OQRXhcZE',
     imagen: 'assets/Images/Movies/7.jpg'
   },
@@ -67,7 +67,7 @@ const peliculas = [
     fecha: '2010',
     categoria: 'Comedia',
     sipnosis:
-      'Desesperado por llegar a casa para acompañar a su esposa embarazada, un arquitecto viaja desde Atlanta hasta Los Ángeles junto a un excéntrico e irritante desconocido. ',
+      'Desesperado por llegar a casa para acompañar a su esposa embarazada, un arquitecto viaja desde Atlanta hasta.',
     trailer: 'https://www.youtube.com/embed/1p3NnJ_oiE0',
     imagen: 'assets/Images/Movies/8.jpg'
   },
@@ -76,7 +76,7 @@ const peliculas = [
     fecha: '2010',
     categoria: 'Comedia',
     sipnosis:
-      'Dave un estudiante de instituto aficionado a los cómics y que siempre pasa desapercibido, decide un día convertirse en superhéroe aunque no tenga superpoderes, no haya seguido un duro entrenamiento y ni siquiera tenga una significativa razón para ello.',
+      'Dave un estudiante de instituto aficionado a los cómics y que siempre pasa desapercibido, decide un día.',
     trailer: 'https://www.youtube.com/embed/Kt39nS-kJ9U',
     imagen: 'assets/Images/Movies/9.jpg'
   },
@@ -85,7 +85,7 @@ const peliculas = [
     fecha: '2017',
     categoria: 'Comedia',
     sipnosis:
-      'Gene, un emoji con varias expresiones, pide a su amigo Hi-5 y al desencriptador Jailbreak que le ayuden a convertirse en un emoji de una cara, como todos sus amigos. Durante su aventura recorren varias aplicaciones y descubren que el teléfono en el que viven está en peligro.',
+      'Gene, un emoji con varias expresiones, pide a su amigo Hi-5 y al desencriptador Jailbreak que le ayuden a.',
     trailer: 'https://www.youtube.com/watch?v=7hKwPHRLz58',
     imagen: 'assets/Images/Movies/10.jpg'
   },
@@ -94,7 +94,7 @@ const peliculas = [
     fecha: '2009',
     categoria: 'Comedia',
     sipnosis:
-      'Una pareja se muda a una mansión para convertirla en un hotel, pero no son conscientes del peligro que les espera. Sin embargo, cuando ella es poseída por un espíritu demoníaco, su compañero decide salvarla.',
+      'Una pareja se muda a una mansión para convertirla en un hotel, pero no son conscientes del peligro que .',
     trailer: 'https://www.youtube.com/watch?v=wnNgGp1KVWQ',
     imagen: 'assets/Images/Movies/11.jpg'
   },
@@ -103,16 +103,16 @@ const peliculas = [
     fecha: '1957',
     categoria: 'Accion',
     sipnosis:
-      'En la Francia de 1916, el general Boulard ordena la conquista alemana y encarga esa misión al general Mireau. Sin embargo, el ataque resulta un fracaso y el alto mando militar decide imponer al regimiento un terrible castigo.',
+      'En la Francia de 1916, el general Boulard ordena la conquista alemana y encarga esa misión al general Mireau.',
     trailer: 'https://www.youtube.com/watch?v=nmDA60X-f_A',
     imagen: 'assets/Images/Movies/12.jpg'
   },
   {
-    titulo: 'LEGO Batman: la película',
+    titulo: 'LEGO Batman:',
     fecha: '2017',
     categoria: 'Ciencia Ficcion',
     sipnosis:
-      'El hijo de Elin muere en un accidente por ahogamiento, pero junto con su hija Storm (10) intenta reiniciar, pero cuando comienzan los rumores de que Storm empujó a su hermano al agua tiene que proteger a su hija y aun así encontrar la verdad.',
+      'El hijo de Elin muere en un accidente por ahogamiento, pero junto con su hija Storm (10) intenta reiniciar, pero.',
     trailer: 'https://www.youtube.com/watch?v=JHxhTOxX_bs',
     imagen: 'assets/Images/Movies/13.jpg'
   },
@@ -121,7 +121,7 @@ const peliculas = [
     fecha: '2022',
     categoria: 'Ciencia Ficcion',
     sipnosis:
-      'Aquaman debe recuperar el legendario Tridente de Atlan para salvar a la ciudad subacuática de Atlantis, y al mundo de la superficie, de su hermano hambriento de poder.',
+      'Aquaman debe recuperar el legendario Tridente de Atlan para salvar a la ciudad subacuática de Atlantis.',
     trailer: 'https://www.youtube.com/embed/D3WCrSnCLEM',
     imagen: 'assets/Images/Movies/14.jpg'
   },
@@ -130,7 +130,7 @@ const peliculas = [
     fecha: '2020',
     categoria: 'Comedia',
     sipnosis:
-      'Sonic intenta atravesar las complejidades de la vida en la Tierra con su nuevo mejor amigo, un humano llamado Tom Wachowski. Deberán unir sus fuerzas para evitar que el malvado Dr. Robotnik capture a Sonic y use sus poderes para dominar el mundo.',
+      'Sonic intenta atravesar las complejidades de la vida en la Tierra con su nuevo mejor amigo, un humano llamado Tom.',
     trailer: 'https://www.youtube.com/watch?v=XPqAai3WBag',
     imagen: 'assets/Images/Movies/15.jpg'
   }
@@ -138,10 +138,10 @@ const peliculas = [
 
 const series = [
   {
-    titulo: "One piece live action",
+    titulo: "One piece",
     fecha: "2023",
     categoria: "Ciencia ficcion",
-    sipnosis: "One Piece  es un manga escrito e ilustrado por el mangaka japonés Eiichirō Oda. Comenzó a publicarse en la revista Japonesa Weekly Shōnen Jump el 22 de julio de 1997 y a la fecha se han publicado 107 volúmenes.",
+    sipnosis: "One Piece  es un manga escrito e ilustrado por el mangaka japonés Eiichirō Oda. Comenzó a publicarse en la revista.",
     trailer: "https://www.youtube.com/embed/4k0YKcqWlBI",
     imagen: "assets/Images/Series/1.jpg"
   },
@@ -149,15 +149,15 @@ const series = [
     titulo: "Merlina",
     fecha: "2022",
     categoria: "Ciencia ficcion",
-    sipnosis: "Miércoles llega a su nuevo colegio, Academia de Nunca Jamás, tras ser expulsada de varios centros. Allí se verá envuelta en una investigación que involucra al pasado de sus padres.",
+    sipnosis: "Miércoles llega a su nuevo colegio, Academia de Nunca Jamás, tras ser expulsada de varios centros. Allí se verá.",
     trailer: "https://www.youtube.com/embed/gmp7LTKnVPo",
     imagen: "assets/Images/Series/2.jpg"
   },
   {
-    titulo: "El juego del calamar",
+    titulo: "juego calamar",
     fecha: "2021",
     categoria: "accion",
-    sipnopsis: "Cientos de personas con dificultades económicas aceptan una extraña invitación a un juego de supervivencia. Les espera un premio millonario, pero hay mucho en juego.",
+    sipnosis: "Cientos de personas con dificultades económicas aceptan una extraña invitación a un juego de.",
     trailer: "https://www.youtube.com/embed/CF2ekNUwbz8",
     imagen: "assets/Images/Series/3.jpg"
   },
@@ -165,7 +165,7 @@ const series = [
     titulo: "Stranger Things",
     fecha: "2016",
     categoria: "Ciencia ficcion",
-    sipnosis: "Cuando un niño desaparece, sus amigos, la familia y la policía se ven envueltos en una serie de eventos misteriosos al tratar de encontrarlo. Su ausencia coincide con el avistamiento de una criatura terrorífica y la aparición de una extraña niña.",
+    sipnosis: "Cuando un niño desaparece, sus amigos, la familia y la policía se ven envueltos en una serie de eventos.",
     trailer: "https://www.youtube.com/embed/KQoYqvc0QVQ",
     imagen: "assets/Images/Series/4.jpg"
   },
@@ -173,7 +173,7 @@ const series = [
     titulo: "Arcane",
     fecha: "2021",
     categoria: "Ciencia ficcion",
-    sipnosis: "Impecable adaptación del mítico videojuego League of Legends (Lol). El argumento se desarrolla en dos ciudades confrontadas, la rica de Piltover y mísera de Zaun. Dos hermanas lucharán en bandos enfrentados cuando se alza la rivalidad entre ambas ciudades en una guerra de tecnologías y creencias contrapuestas.",
+    sipnosis: "Impecable adaptación del mítico videojuego League of Legends (Lol). El argumento se desarrolla en dos.",
     trailer: "https://www.youtube.com/embed/Mj4_xG5NXeo",
     imagen: "assets/Images/Series/5.jpg"
   },
@@ -189,7 +189,7 @@ const series = [
     titulo: "La Casa De Papel",
     fecha: "2017",
     categoria: "accion",
-    sipnosis: "Una banda organizada de ladrones se propone cometer el atraco del siglo en la Fábrica Nacional de Moneda y Timbre. Cinco meses de preparación quedarán reducidos a once días para poder llevar a cabo con éxito el gran golpe.",
+    sipnosis: "Una banda organizada de ladrones se propone cometer el atraco del siglo en la Fábrica Nacional de Moneda y.",
     trailer: "https://www.youtube.com/embed/xPKN7MxS8TU",
     imagen: "assets/Images/Series/7.jpg"
   },
@@ -197,7 +197,7 @@ const series = [
     titulo: "Dark",
     fecha: "2017",
     categoria: "Ciencia ficcion",
-    sipnosis: "La historia arranca con la desaparición de un niño en una pequeña localidad alemana, hecho que cambiará las vidas de cuatro familias que viven allí.",
+    sipnosis: "La historia arranca con la desaparición de un niño en una pequeña localidad alemana, hecho que cambiará.",
     trailer: "https://www.youtube.com/embed/SrPggvIrRBI",
     imagen: "assets/Images/Series/8.jpg"
   },
@@ -205,7 +205,7 @@ const series = [
     titulo: "Wild Wild Country",
     fecha: "2018",
     categoria: "accion",
-    sipnosis: "Un controvertido líder de culto construye una ciudad utópica en el desierto de Oregón. El conflicto con los lugareños se convierte en un escándalo nacional.",
+    sipnosis: "Un controvertido líder de culto construye una ciudad utópica en el desierto de Oregón. El conflicto con los.",
     trailer: "https://www.youtube.com/embed/3y-6iaveY6c",
     imagen: "assets/Images/Series/9.jpg"
   },
@@ -213,7 +213,7 @@ const series = [
     titulo: "Breaking bad ",
     fecha: "2008",
     categoria: "accion",
-    sipnosis: "Walter White es un profesor de química de un instituto de Albuquerque. Cuando cumple 50 años es diagnosticado de cáncer en fase terminal. Por ello, el hombre decide aventurarse en el negocio de la droga para solventar las deudas de su familia.",
+    sipnosis: "Walter White es un profesor de química de un instituto de Albuquerque. Cuando cumple 50 años es diagnosticado de.",
     trailer: "https://www.youtube.com/embed/IJ_AZCvCacw",
     imagen: "assets/Images/Series/10.jpg"
   },
@@ -221,7 +221,7 @@ const series = [
     titulo: "Vis a vis",
     fecha: "2015",
     categoria: "accion",
-    sipnosis: "La ficción gira entorno a Macarena, una joven inofensiva que ingresa en la prisión de Cruz del Sur para cumplir condena por malversación en la empresa donde trabaja. La muchacha tiene que cambiar su actitud cuando conoce a sus compañeras de celda y comienza a experimentar desagradables vivencias.",
+    sipnosis: "La ficción gira entorno a Macarena, una joven inofensiva que ingresa en la prisión de Cruz del Sur para.",
     trailer: "https://www.youtube.com/embed/hBLS_OM6Puk",
     imagen: "assets/Images/Series/12.jpg"
   },
@@ -229,7 +229,7 @@ const series = [
     titulo: "Peaky Blinders",
     fecha: "2013",
     categoria: "accion",
-    sipnosis: "Gran Bretaña vive la posguerra. Los soldados regresan, se acuñan nuevas revoluciones y nacen bandas criminales en una nación agitada. En Birmingham, una pandilla de gánsters callejeros asciende hasta convertirse en los reyes de la clase obrera.",
+    sipnosis: "Gran Bretaña vive la posguerra. Los soldados regresan, se acuñan nuevas revoluciones y nacen bandas.",
     trailer: "https://www.youtube.com/embed/HhesaQXLuRY",
     imagen: "assets/Images/Series/13.jpg"
   },
@@ -237,15 +237,15 @@ const series = [
     titulo: "Narcos",
     fecha: "2015",
     categoria: "accion",
-    sipnosis: "La historia del ascenso y caída del capo colombiano Pablo Escobar y de los agentes de la DEA que lucharon para atraparlo. Se intenta enfocar la narración de manera ecuánime desde el punto de vista del propio Escobar y del agente de la DEA Steve Murphy, mostrando los lados opuestos de lo que se convertiría en un guerra cruda y total.",
+    sipnosis: "La historia del ascenso y caída del capo colombiano Pablo Escobar y de los agentes de la DEA que.",
     trailer: "https://www.youtube.com/embed/1AZOY0Xo3m8",
     imagen: "assets/Images/Series/14.jpg"
   },
   {
-    titulo: "Monstruo: La historia de Jeffrey Dahmer",
+    titulo: "Monstruo",
     fecha: "2022",
     categoria: "accion",
-    sipnosis: "La historia de uno de los asesinos en serie más conocidos de Estados Unidos, contada desde los puntos de vista de sus víctimas. ",
+    sipnosis: "La historia de uno de los asesinos en serie más conocidos de Estados Unidos, contada desde los puntos de . ",
     trailer: "https://www.youtube.com/embed/ZHZG6UArE3I",
     imagen: "assets/Images/Series/15.jpg"
   },
@@ -253,7 +253,7 @@ const series = [
     titulo: "The crown",
     fecha: "2021",
     categoria: "accion",
-    sipnosis: "Cientos de personas con dificultades económicas aceptan una extraña invitación a un juego de supervivencia. Les espera un premio millonario, pero hay mucho en juego.",
+    sipnosis: "Cientos de personas con dificultades económicas aceptan una extraña invitación a un juego de.",
     trailer: "https://www.youtube.com/embed/xl8zdCY-abw",
     imagen: "assets/Images/Series/11.jpg"
   }
@@ -296,7 +296,7 @@ const crearCarta = (pelicula) => {
   const trailer = document.createElement('a')
   trailer.setAttribute('target', '_blank')
   trailer.classList.add('trailer')
-  trailer.textContent = 'Ver trailer'
+  trailer.textContent = 'VER TRAILER'
   trailer.href = pelicula.trailer
 
   element.appendChild(img)
@@ -408,9 +408,6 @@ filtroCategoria.addEventListener('change', () => {
     filtrarPeliculasPorCategoria(categoria);
     filtrarSeriesPorCategoria(categoria);
   }
-
-  
-
 })
 
 
